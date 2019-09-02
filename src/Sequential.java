0def51611ad888ac2e72d892254cf3c92f9f13a1
a3y3 <sohamssd@gmail.com>
2019-09-02 11:34:59 -0400

    Print the *smallest* p for each i such that i = p+2q.
    -However, is this the only way of proving the conjecture? The complexity is O((range)*(p)*(p)); p being number of primes LESS THAN range, which is too high.

--- src/Sequential.java
+++ src/Sequential.java
@@ -4,26 +4,22 @@
 public class Sequential {
     private int low;
     private int high;
-    private Prime.Iterator iterator;
 
-    Sequential(){
+    Sequential() {
         low = 2;
         high = 2;   //Default low and high values to lowest valid prime
-        iterator = new Prime.Iterator();
     }
 
     public static void main(String[] args) {
         Sequential sequential = new Sequential();
         sequential.parseArguments(args);
-
-
+        sequential.scanOddNumbers();
     }
 
     /**
      * Parses arguments and sets high and low values.
      *
      * @param args arguments from STDIN
-     *
      * @throws IllegalStateException if low > high, ie. args[0] > args[1].
      */
     private void parseArguments(String[] args) {
@@ -35,5 +31,22 @@ public class Sequential {
         }
     }
 
+    private void scanOddNumbers() {
+        low = low % 2 == 0 ? low + 1 : low;
+        high = high % 2 == 0 ? high - 1 : high;  //Make sure low and high are odd
+        for (int i = low; i <= high; i += 2) {
+            Prime.Iterator pIterator = new Prime.Iterator();
+            Prime.Iterator qIterator = new Prime.Iterator();
+            int p = pIterator.next(), q = qIterator.next();
 
+            for (; p + (2 * q) != i; p = pIterator.next()) {
+                while ((p + (2 * q)) < i) {
+                    q = qIterator.next();
+                }
+                qIterator.restart();
+                q = qIterator.next();
+            }
+            System.out.println(i + " = " + p + " + 2*" + q);
+        }
+    }
 }
