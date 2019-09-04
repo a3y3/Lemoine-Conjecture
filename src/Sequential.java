19a374aef6f09774c98d1c2d77017b928b82af4e
a3y3 <sohamssd@gmail.com>
2019-09-04 08:30:41 -0400

    Add verbose outputs (and javadoc)

--- src/Sequential.java
+++ src/Sequential.java
@@ -5,9 +5,13 @@ public class Sequential {
     private int low;
     private int high;
 
-    Sequential() {
+    // if enabled using -v, print p and q for all numbers in range
+    private boolean verboseOutputs;
+
+    private Sequential() {
         low = 2;
         high = 2;   //Default low and high values to lowest valid prime
+        verboseOutputs = false; // Default behaviour prints only highest prime
     }
 
     public static void main(String[] args) {
@@ -29,14 +33,38 @@ public class Sequential {
             throw new IllegalStateException("low cannot exceed high, check " +
                     "arguments");
         }
+        for (int i = 2; i < args.length; i++){
+            if (args[i].equals("-v")){
+                verboseOutputs = true;
+                break;      //For performance, MUST remove if adding more flags.
+            }
+        }
     }
 
+    /**
+     * Contains a for loop that iterators over odd numbers, and calls @code{getPrimes
+     * (i)} for each @code{i} in the loop.
+     * Depending on the value of @code{verboseOutputs}, the value "i=p+2q" is printed.
+     * If @code{verboseOutputs} is false (default), only the highest @code{i} is printed.
+     */
     private void scanOddNumbers() {
         low = low % 2 == 0 ? low + 1 : low;
         high = high % 2 == 0 ? high - 1 : high;  //Make sure low and high are odd
+        int maxP = -1;
+        int finalQ = -1;
+        int finalNumber = - 1;
         for (int i = low; i <= high; i += 2) {
             int[] pAndQ = getPrimes(i);
+            if (maxP < pAndQ[0]){
+                maxP = pAndQ[0];
+                finalQ = pAndQ[1];
+                finalNumber = i;
+            }
+            if (verboseOutputs){
+                System.out.println(i + " = " + pAndQ[0] + " + 2*" + pAndQ[1]);
+            }
         }
+        System.out.println(finalNumber + " = " + maxP + " + 2*" + finalQ);
     }
 
     /**
@@ -63,7 +91,6 @@ public class Sequential {
             qIterator.restart();
             q = 2;
         }
-        System.out.println(number + " = " + p + " + 2*" + q);
         return new int[]{p, q};
     }
 }
